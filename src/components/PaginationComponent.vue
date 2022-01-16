774968359f910bb62241f83859888729e0b443bf
eternal <eternaltalaba@gmail.com>
2022-01-16 13:48:36 +0500

    pagination component

---
+++ src/components/PaginationComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="page__wrapper">
+    <div
+      v-for="pageNumber in totalPages"
+      :key="pageNumber"
+      class="page"
+      :class="{
+        'current-page': page === pageNumber,
+      }"
+      @click="sendPage(pageNumber)"
+    >
+      {{ pageNumber }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, } from "vuex";
+export default {
+  methods: {
+    sendPage(pageNumber) {
+      this.$emit("sendPage", pageNumber);
+    },
+  },
+    computed: {
+    ...mapState({
+      page: (state) => state.post.page,
+      totalPages: (state) => state.post.totalPages,
+    }),
+  },
+};
+</script>
+
+<style scoped>
+.page__wrapper {
+  display: flex;
+  margin: 15px;
+}
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+.current-page {
+  border: 2px solid red;
+  font-size: x-large;
+}
+</style>
--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -1,34 +1,25 @@
 <template>
   <div>
     <h1>Page with the posts</h1>
-
     <post-list :posts="posts" />
-  </div>
-  <div class="page__wrapper">
-    <div
-      v-for="pageNumber in totalPages"
-      :key="pageNumber"
-      class="page"
-      :class="{
-        'current-page': page === pageNumber,
-      }"
-      @click="changePage(pageNumber)"
-    >
-      {{ pageNumber }}
-    </div>
+    <pagination-component @sendPage="changePage"></pagination-component>
   </div>
 </template>
 
 <script>
 import PostList from "@/components/PostList";
-import { mapState, mapActions } from "vuex";
+import PaginationComponent from "@/components/PaginationComponent.vue";
+import { mapActions, mapState } from "vuex";
 export default {
-  components: { PostList },
-  data() {
-    return {
-    };
+  components: { PostList, PaginationComponent },
+  computed: {
+    ...mapState({
+      posts: (state) => state.post.posts,
+      page: (state) => state.post.page,
+      limit: (state) => state.post.limit,
+      totalPages: (state) => state.post.totalPages,
+    }),
   },
-
   methods: {
     ...mapActions({
       fetchPosts: "post/fetchPosts",
@@ -43,28 +34,5 @@ export default {
   mounted() {
     this.fetchPosts();
   },
-  computed: {
-    ...mapState({
-      posts: (state) => state.post.posts,
-      page: (state) => state.post.page,
-      limit: (state) => state.post.limit,
-      totalPages: (state) => state.post.totalPages,
-    }),
-  },
 };
 </script>
-
-<style scoped>
-.page__wrapper {
-  display: flex;
-  margin: 15px;
-}
-.page {
-  border: 1px solid black;
-  padding: 10px;
-}
-.current-page {
-  border: 2px solid red;
-  font-size: x-large;
-}
-</style>
